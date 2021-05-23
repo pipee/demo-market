32ccbb8d5b01d2c456dd7f7e74225cc211c17e39
Felipe Armijo <luis.armijo@mayor.cl>
2021-05-23 01:30:46 -0400

    producto repository

---
+++ src/main/java/com/pipe/demomarket/persistence/ProductoRepository.java
@@ -0,0 +1,24 @@
+package com.pipe.demomarket.persistence;
+
+import com.pipe.demomarket.persistence.crud.ProductoCrudRepository;
+import com.pipe.demomarket.persistence.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+}
